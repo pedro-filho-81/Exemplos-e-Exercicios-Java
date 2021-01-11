d3ede7171b0c787d6c11646d37ad6ce61cb11a93
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-11 11:06:14 -0300

    Teste
    Programa para mostrar valores de duas variáveis

---
+++ JavaParaIniciantes/Cap01/Exemplos/Teste/src/teste/Teste.java
@@ -0,0 +1,29 @@
+package teste;
+
+/**
+ * 11/01/2021
+ */
+
+// classe
+public class Teste {
+
+    // função principal
+    public static void main(String[] args) {
+        
+        // variáveis
+        int var1 = 1024; // var1 recebe valor
+        int var2; // var2 sem valor
+        
+        // mostra a variável var1
+        System.out.println( "var1 = " + var1 );
+        
+        // var2 recebe valor
+        var2 = var1 / 2;
+        
+        // mostra var2
+        System.out.println("var2 = " + var2 );
+        
+        
+    }
+    
+}

9fbc037be2fd1310c97409f200497c0ab08a849d
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-11 10:25:44 -0300

    Olá Mundo Java
    Primeiro exemplo java, imprimir olá mundo na tela.

---
+++ Cap02/Exemplos/OlaMundoJava.java
@@ -0,0 +1,14 @@
+package olamundojava;
+/*  Primeiro programa em java
+Autor: Pedro Filho, 11/01/2021,
+*/
+// classe olá Mundo
+public class OlaMundoJava {
+
+    // função principal
+    public static void main(String[] args) {
+        
+        // imprimir
+        System.out.println( "Olá¡, Mundo Java!" );        
+    } // fim função principal    
+} // fim classe
